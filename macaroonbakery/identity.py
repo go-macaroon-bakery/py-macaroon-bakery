3825d6030b73c5a086626a7dd82dec51b1c26bd7
fabricematrat <fabricematrat@gmail.com>
2017-09-01 15:42:12 +0200

    Add context to authorizer.

--- macaroonbakery/identity.py
+++ macaroonbakery/identity.py
@@ -37,9 +37,10 @@ class ACLIdentity(Identity):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def allow(self, acls):
+    def allow(self, ctx, acls):
         ''' reports whether the user should be allowed to access
         any of the users or groups in the given acl list.
+        :param ctx(AuthContext) is the context of the authorization request.
         :param acls array of string acl
         :return boolean
         '''
